472c471f26d5d45498754fdfceeb7315478621f2
onib0002 <onib0002@algonquinlive.com>
2021-11-14 18:34:20 -0500

    collection reference and getdocument created

--- src/index.js
+++ src/index.js
@@ -17,3 +17,20 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig)
 const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'users')
+
+// get collection data
+getDocs(colRef)
+  .then(snapshot => {
+    // console.log(snapshot.docs)
+    let users = []
+    snapshot.docs.forEach(doc => {
+      users.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(users)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
